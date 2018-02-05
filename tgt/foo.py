3d9900c71ef0c4feb5d042b9e90b8a4addb9a6fc
ferria <ferria@rpi.edu>
2018-02-04 23:23:00 -0500

    added __init__ and stub files

---
+++ tgt/foo.py
@@ -0,0 +1,10 @@
+"""
+This is a test file to evaluate development tools
+"""
+
+
+def hello_world(x: int) -> str:
+    return str(x) + str(x)
+
+if __name__ == '__main__':
+    print("Hello world, {0}".format(hello_world(7)))
