0621f54b6ed4ac86a18e50848e6a4bd094097e54
ferria <ferria@rpi.edu>
2018-02-04 23:33:26 -0500

    updated travis script commands

--- tgt/foo.py
+++ tgt/foo.py
@@ -6,5 +6,6 @@ This is a test file to evaluate development tools
 def hello_world(x: int) -> str:
     return str(x) + str(x)
 
+
 if __name__ == '__main__':
     print("Hello world, {0}".format(hello_world(7)))
